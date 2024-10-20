874a3fbae3c3ba1761ed9246424e6ebc8a8688bb
ReXDeX123 <92604823+ReXDeX123@users.noreply.github.com>
2024-10-20 11:07:49 +0200

    Add files via upload

---
+++ prime numbers for github daily.py
@@ -0,0 +1,20 @@
+def prime_numbers(n):
+    number=[x for x in range(n+1)]
+    number.pop(0)
+    number.pop(0)
+    for x in number:
+        for i in range(2,n+1):
+            try:
+                a=i*x
+                number.remove(a)
+            except:
+                continue
+    return number
+                
+
+
+
+
+
+
+
